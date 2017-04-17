159bbd7de3953a36a9835ba3a7a0938a6d66b9f0
Casey Duplantis <cduplantis@gmail.com>
2017-04-17 07:23:59 -0500

    finalcountdown and new layout
    This is a broken branch for meow.

--- keyboards/planck/keymaps/bayoucaneboy/keymap.c
+++ keyboards/planck/keymaps/bayoucaneboy/keymap.c
@@ -16,7 +16,7 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 1
-#define _COLEMAK 0
+#define _COLEMAK 2
 #define _LOWER 3
 #define _RAISE 4
 #define _ADJUST 16
@@ -32,29 +32,108 @@ enum planck_keycodes {
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
-#define TD_SEMI_COLON   1
-#define TD_QUOT_DBLQUOT 2
+
+
+// Double Tap Action codes; see below
+#define TDA_SEMI_COLON   1
+#define TDA_QUOT_DBLQUOT 2
+
+// Double Tap code to make map easier to read
+#define TD_SCLN TD(TDA_SEMI_COLON)
+#define TD_QUOT TD(TDA_QUOT_DBLQUOT)
+
+// LOWER TAPS
+#define LT_SPC  LT(LOWER, KC_SPC)
+
+// MOD TAPS
+#define LT_SPC  LT(LOWER, KC_SPC)
+#define MT_RSFT_ENT MT(MOD_RSFT, KC_ENT)
+
+
+#ifdef AUDIO_ENABLE
+
+// C# B C# F# | - . D C# D C# B
+// |-. D C# D F# | -. B A B A G# B | A G# A B A B C# B A G# F D C# C# C# D C# B C#
+#define FNL_CTDN MC(1)
+#define FINAL_COUNTDOWN_SOUND  \
+S__NOTE(_CS5  ),      \
+S__NOTE(_B5  ),      \
+Q__NOTE(_CS5  ),      \
+Q__NOTE(_FS4  ),      \
+\
+QD_NOTE(REST  ),      \
+S__NOTE(_D5  ),      \
+S__NOTE(_CS5  ),      \
+E__NOTE(_D5  ),      \
+E__NOTE(_CS5  ),      \
+Q__NOTE(_B5  ),      \
+\
+QD_NOTE(REST  ),      \
+S__NOTE(_D5  ),      \
+S__NOTE(_CS5  ),      \
+Q__NOTE(_D5  ),      \
+Q__NOTE(_FS4  ),      \
+\
+QD_NOTE(REST  ),      \
+S__NOTE(_B5  ),      \
+S__NOTE(_A5  ),      \
+E__NOTE(_B5  ),      \
+E__NOTE(_A5  ),      \
+E__NOTE(_GS4  ),      \
+E__NOTE(_B5  ),      \
+\
+QD_NOTE(_A5  ),      \
+S__NOTE(_GS4  ),      \
+S__NOTE(_A5  ),      \
+\
+QD_NOTE(_B5  ),      \
+S__NOTE(_A5  ),      \
+S__NOTE(_B5  ),      \
+\
+E__NOTE(_CS5  ),      \
+E__NOTE(_B5  ),      \
+E__NOTE(_A5  ),      \
+E__NOTE(_GS4  ),      \
+Q__NOTE(_F4  ),      \
+Q__NOTE(_D5  ),      \
+\
+HD_NOTE(_CS5  ),      \
+S__NOTE(_CS5  ),      \
+S__NOTE(_D5  ),      \
+S__NOTE(_CS5  ),      \
+E__NOTE(_B5  ),      \
+\
+W_NOTE(_CS5  ),
+
+float tone_startup[][2]         = SONG(STARTUP_SOUND);
+float tone_qwerty[][2]          = SONG(QWERTY_SOUND);
+float tone_colemak[][2]         = SONG(COLEMAK_SOUND);
+float music_scale[][2]          = SONG(MUSIC_SCALE_SOUND);
+float tone_final_countdown[][2] = SONG(FINAL_COUNTDOWN_SOUND);
+float tone_goodbye[][2]         = SONG(GOODBYE_SOUND);
+#endif
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
 /* Qwerty
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |   Q  |   W  |   E  |   R  |   T  |  Esc |Enter |   Y  |   U  |   I  |   O  |   P  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |SHEnt |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  | Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * |   A  |   S  |   D  |   F  |   G  |  {[  |  ]}  |   H  |   J  |   K  |   L  |   ;  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |   Z  |   X  |   C  |   V  |   B  |  DEL |  '   |   N  |   M  |   ,  |   .  |   /  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | SHIFT|      | Ctrl | Alt  | GUI  |  Space/Low  | RAISE|      |      |      |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    TD(TD_SEMI_COLON), TD(TD_QUOT_DBLQUOT)},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT) },
-  {KC_DEL,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+  {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_ESC,  KC_ENT,  KC_Y,  KC_U,    KC_I,    KC_O,    KC_P},
+  {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LCBR, KC_RCBR, KC_H,  KC_J,    KC_K,    KC_L,    TD_SCLN},
+  {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_DEL,  TD_QUOT, KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH},
+  {KC_LSFT, _______, KC_LCTL, KC_LALT, KC_LGUI, LT_SPC/*------*/, RAISE, _______, _______, _______, MT_RSFT_ENT}
 },
+
 
 /* Colemak
  * ,-----------------------------------------------------------------------------------.
@@ -76,21 +155,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   7  |   8  |   9  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
+ * |      | Right|  Up  | Left | PgUp |  (   |   )  |   +  |   4  |   5  |   6  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
+ * | BACLT| Home | Down | End  | PgDn | Bksp |   |  |   -  |   1  |   2  |   3  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * | SHIFT|      | Ctrl | Alt  | GUI  |  Space/Low  | RAISE|   +  |   0  |   .  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______},
-  {BACKLIT, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+  {KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_7,    KC_8,    KC_9,    _______},
+  {_______, KC_LEFT, KC_UP,    KC_RGHT, KC_PGUP, KC_LPRN, KC_RPRN, KC_PLUS, KC_4,    KC_5,    KC_6,    _______},
+  {BACKLIT, KC_HOME, KC_DOWN,  KC_END,  KC_PGDN, KC_BSPC, KC_PIPE, KC_MINS, KC_1,    KC_2,    KC_3,    _______},
+  {_______, _______, _______,  _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 },
+
+
+/* Raise
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |      |      |      |      |      |      |      |  F9  |  F10 |  F11 |  F12 |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * |      |      |      |      |      |   <  |   >  |      |  F5  |  F6  |  F7  |  F8  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |  F1  |  F2  |  F3  |  F4  |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | SHIFT|      | Ctrl | Alt  | GUI  |  Space/Low  | RAISE| Next | Vol- | Vol+ | Play |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_RAISE] = {
+  {KC_GRV,  _______, _______, _______, _______, _______, _______, _______, KC_F9,   KC_F10,  KC_F11,  KC_F12},
+  {KC_DEL,  _______, _______, _______, _______, KC_LABK, KC_RABK, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4},
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+},
+
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
@@ -112,7 +211,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * |      | Reset|      |      |      |      |      |      |      |      |      |  Del |
+ * |      | Reset|      |      |      |      |      |      |      |      |      |  FNL_CTDN |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |Aud on|Audoff|      |      |Qwerty|Colemk|      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -122,24 +221,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = {
-  {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL},
+  {_______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, FNL_CTDN},
   {_______, _______, _______, AU_ON,   AU_OFF,  _______, _______, QWERTY,  COLEMAK, _______, _______, _______},
   {_______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______},
-  {_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______}
+  {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
 }
+
 
 
 };
 
-#ifdef AUDIO_ENABLE
-
-float tone_startup[][2]    = SONG(STARTUP_SOUND);
-float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
-float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
-float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
-
-float tone_goodbye[][2] = SONG(GOODBYE_SOUND);
-#endif
 
 
 void persistant_default_layer_set(uint16_t default_layer) {
@@ -149,8 +240,8 @@ void persistant_default_layer_set(uint16_t default_layer) {
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   // action to do when TD(1) is tap-danced
-  [TD_SEMI_COLON] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, LSFT(KC_SCLN)),
-  [TD_QUOT_DBLQUOT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, LSFT(KC_QUOT))
+  [TDA_SEMI_COLON] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, LSFT(KC_SCLN)),
+  [TDA_QUOT_DBLQUOT] = ACTION_TAP_DANCE_DOUBLE(KC_QUOT, LSFT(KC_QUOT))
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -240,3 +331,22 @@ void music_scale_user(void)
 }
 
 #endif
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) // this is the function signature -- just copy/paste it into your keymap file as it is.
+{
+  switch(id) {
+    case 0: // this would trigger when you hit a key mapped as M(0)
+      if (record->event.pressed) {
+        return MACRO( I(255), T(H), T(E), T(L), T(L), W(255), T(O), END  ); // this sends the string 'hello' when the macro executes
+      }
+      break;
+     case 1:
+       if (record->event.pressed) {
+          #ifdef AUDIO_ENABLE
+            PLAY_NOTE_ARRAY(tone_final_countdown, false, 0);
+          #endif
+         return MACRO_NONE;
+       }
+  }
+  return MACRO_NONE;
+};
